7aab3447c92f01bf3c394879eb491942d1d600e5
SaurabPoudel <poudelsaurab20@gmail.com>
2023-11-07 23:40:25 +0545

    Almost done

---
+++ src/components/NewNote.tsx
@@ -0,0 +1,19 @@
+import { NoteData, Tag } from "../App.tsx"
+import NoteForm from "./NoteForm.tsx"
+
+type NewNoteProps = {
+  onSubmit: (data: NoteData)  => void
+  onAddTag: (tag: Tag) => void
+  availableTags: Tag[]
+}
+
+const NewNote = ({onSubmit, onAddTag, availableTags} : NewNoteProps) => {
+  return (
+    <>
+        <h1 className="mb-4">New Note</h1>
+        <NoteForm onSubmit={onSubmit} onAddTag={onAddTag} availableTags={availableTags} />
+    </>
+  )
+}
+
+export default NewNote
